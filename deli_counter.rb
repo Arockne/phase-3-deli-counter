bf09420d9384fe5a0efbd381ff785df76acf0403
Daniel Calvin <arockne2003@yahoo.com>
2021-09-29 15:31:20 -0700

    commit tree ba0e1a806d52cd6e797ee8e3a977bd5668a0ba09

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,8 +1,18 @@
-katz_deli = ["Ada", "Bob", "Tom"]
+katz_deli = []
 
 def line(deli)
-  return "The line is currently empty." if deli.empty?
+  return puts "The line is currently empty." if deli.empty?
 
-  deli.map.with_index { |person, i| "#{i+1}. #{person}"}.join(' ')
+  current_line = deli.map.with_index { |person, i| "#{i+1}. #{person}"}.join(' ')
+  puts "The line is currently: #{current_line}"
 end
 
+def take_a_number(line, person)
+  line << person
+  puts "Welcome, #{person}. You are number #{line.length} in line."
+end
+
+def now_serving(line)
+  return puts "There is nobody waiting to be served!" if line.length == 0
+  puts "Currently serving #{line.shift}."
+end
